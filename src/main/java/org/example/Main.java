f369eed500550f3e2cd39fd36c740c1fa065d030
lukpro681 <lukapro681@student.polsl.pl>
2024-05-13 14:15:51 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+
+
+    public int added(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import junit.framework.TestCase;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest extends TestCase{
+
+
+    public void testAdded() {
+        Main main = new Main();
+        assertEquals(10,main.added(4,6));
+        }
+    }
