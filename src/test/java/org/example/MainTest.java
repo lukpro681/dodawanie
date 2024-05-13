94bed54e9a69d2a550dbcc16b38779ae6fe1d464
lukpro681 <lukapro681@student.polsl.pl>
2024-05-13 15:09:32 +0200

    Commited 2

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,10 +1,12 @@
 package org.example;
 
 import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
 
 
 public class MainTest extends TestCase{
 
+    @Test
     public void testAdded() {
         Main main = new Main();
         assertEquals(10,main.added(4,7));
